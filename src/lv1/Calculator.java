75d5107705887284d7161f682cb72bf72ee56635
kimwanyoung <dhks2869@gmail.com>
2024-09-04 12:36:01 +0900

    feat : 양의 정수 검증 구현

--- src/lv1/Calculator.java
+++ src/lv1/Calculator.java
@@ -3,6 +3,7 @@ package lv1;
 import java.util.Scanner;
 
 public class Calculator {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -18,5 +19,14 @@ public class Calculator {
         // 연산자 입력받기
         System.out.print("사칙연산 기호를 입력하세요: ");
         String operator = sc.nextLine();
+
+        // 양의 정수 검증
+        validatePositiveInteger(firstNumber, secondNumber);
+    }
+
+    static void validatePositiveInteger(int firstNumber, int secondNumber) {
+        if(firstNumber < 0 || secondNumber < 0) {
+            throw new IllegalArgumentException("[Error] : 양의 정수만 입력 가능합니다.");
+        }
     }
 }

5b276b8d42c17d6e483818261c0c8122ca02f3c1
kimwanyoung <dhks2869@gmail.com>
2024-09-04 12:29:44 +0900

    feat : 양의 정수 입력받기

---
+++ src/lv1/Calculator.java
@@ -0,0 +1,22 @@
+package lv1;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 두 정수 입력받기
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int firstNumber = sc.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int secondNumber = sc.nextInt();
+
+        // 남아있는 개행문자 소비
+        sc.nextLine();
+
+        // 연산자 입력받기
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        String operator = sc.nextLine();
+    }
+}

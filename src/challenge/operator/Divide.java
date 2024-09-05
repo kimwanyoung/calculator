c3ff95e748850723ef8eff358b07080eff5676bd
kimwanyoung <dhks2869@gmail.com>
2024-09-05 19:36:22 +0900

    fix : 0으로 나누었을 때 예외 발생이 아닌 Infinity 반환 수정

--- src/challenge/operator/Divide.java
+++ src/challenge/operator/Divide.java
@@ -3,10 +3,7 @@ package challenge.operator;
 public class Divide extends AbstractOperator<Number> {
     @Override
     public Number operate(Number firstNumber, Number secondNumber) {
-        try {
-            return firstNumber.doubleValue() / secondNumber.doubleValue();
-        } catch (ArithmeticException exception) {
-            throw new ArithmeticException("0으로 나눌 수 없습니다.");
-        }
+        if(secondNumber.equals(0)) throw new ArithmeticException("0으로 나눌 수 없습니다.");
+         return firstNumber.doubleValue() / secondNumber.doubleValue();
     }
 }
